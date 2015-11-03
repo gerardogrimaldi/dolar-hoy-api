835738fbd1f2ce96d3300d7099ce41de7a64e72a
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2015-11-03 16:33:29 -0300

    more fixes

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
-//
-// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
-// Mongoose is a object/data mapping utility for the MongoDB database.
-//
-
-// by Ben Wen with thanks to Aaron Heckmann
-
-//
-// Copyright 2015 ObjectLabs Corp.  
-// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
-//
-// MIT Licensed
-//
-
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation files
-// (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of the Software,
-// and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:  
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software. 
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE. 
-
-//
-// Preamble
 var http = require ('http');	     // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.  
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost/HelloMongoose';
+    'mongodb://heroku_hp49qmbd:2uac8h9g06op5db8a1d3i26kr0@ds049084.mongolab.com:49084/heroku_hp49qmbd';
 
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
+
+var app = express();
+app.use(express.logger());
+
+// CORS
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-var uriString = 'mongodb://heroku_hp49qmbd:2uac8h9g06op5db8a1d3i26kr0@ds049084.mongolab.com:49084/heroku_hp49qmbd';
-var mongoose = require ("mongoose");
+var uriString = process.env.MONGOLAB_URI;
+var mongoose = require('mongoose');
 var express = require('express');
-var mail = require("./nodemail");
-var valoresSchema = require("./Model/mongoSchema").valoresDolarHoySchema;
+var mail = require('./nodemail');
+var valoresSchema = require('./Model/mongoSchema').valoresDolarHoySchema;
 var Valores = mongoose.model('ValoresDolarHoy', valoresSchema);
+
+mongoose.connect(uriString, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uriString + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uriString);
+  }
+});
 
 var app = express();
 app.use(express.logger());
-
 // CORS
 app.all('/*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -21,31 +28,17 @@ app.get('/dolar/:pass', function(req, res) {
   if (req.params.pass !== 'Hola123!') {
     return res.send('Error: Wrong password...');
   }
-
-  try {
-    if(!mongoose.connection.readyState) {
-      mongoose.connect(uriString, function (err, response) {
+  Valores.findOne()
+    .select('dolarCompra dolarVenta dolarBlueCompra dolarBlueVenta dolarTarjeta realCompra realVenta euroCompra euroVenta date')
+    .sort('-date')
+    .exec(
+      function (err, doc) {
+        console.log(doc);
         if (err) {
-          onError('ERROR connecting to: ' + uriString + '. ' + err);
-        } else {
-          console.log ('Succeeded connected to: ' + uriString);
-          Valores.findOne()
-            .select('dolarCompra dolarVenta dolarBlueCompra dolarBlueVenta dolarTarjeta realCompra realVenta euroCompra euroVenta date')
-            .sort('-date')
-            .exec(
-            function (err, doc) {
-              console.log(doc);
-              if (err) {
-                return onError(err);
-              }
-              return res.send(JSON.stringify(doc));
-            });
+          return onError(err);
         }
+        return res.send(JSON.stringify(doc));
       });
-    }
-  } catch (err) {
-    onError(err);
-  }
 });
 
 app.listen(process.env.PORT || 3000);
