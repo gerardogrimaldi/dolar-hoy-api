849b5569dcff4fe6afd932bf2caad26349d66e9e
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2015-11-03 15:59:57 -0300

    app

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
+//
+// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
+// Mongoose is a object/data mapping utility for the MongoDB database.
+//
 
+// by Ben Wen with thanks to Aaron Heckmann
+
+//
+// Copyright 2015 ObjectLabs Corp.  
+// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
+//
+// MIT Licensed
+//
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:  
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software. 
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+
+//
+// Preamble
 var http = require ('http');	     // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 
 // Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
+// localhost if we don't find one.  
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://heroku_hp49qmbd:2uac8h9g06op5db8a1d3i26kr0@ds049084.mongolab.com:49084/heroku_hp49qmbd';
+    'mongodb://localhost/HelloMongoose';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
@@ -50,7 +85,7 @@ var johndoe = new PUser ({
   age: 25
 });
 
-// Saving it to the database.
+// Saving it to the database.  
 johndoe.save(function (err) {if (err) console.log ('Error on save!')});
 
 // Creating more users manually
@@ -73,8 +108,8 @@ alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
 var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
 
 // Create a rudimentary http server.  (Note, a real web application
-// would use a complete web framework and router like express.js).
-// This is effectively the main interaction loop for the application.
+// would use a complete web framework and router like express.js). 
+// This is effectively the main interaction loop for the application. 
 // As new http requests arrive, the callback function gets invoked.
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -106,3 +141,26 @@ function createWebpage (req, res) {
 // The listener in http.createServer should still be active after these messages are emitted.
 console.log('http server will be listening on port %d', theport);
 console.log('CTRL+C to exit');
+
+//
+// House keeping.
+
+//
+// The rudimentary HTML content in three pieces.
+var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
+<head> \
+<style> body {color: #394a5f; font-family: sans-serif} </style> \
+</head> \
+<body> \
+<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
+See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
+<br\> \
+<br\> \
+<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
+var html2 = '</code></pre> <br\> <i>';
+var html3 = ' documents. </i> <br\> <br\>';
+var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
+var html5 = '</code></pre> <br\> <i>';
+var html6 = ' documents. </i> <br\> <br\> \
+<br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+
